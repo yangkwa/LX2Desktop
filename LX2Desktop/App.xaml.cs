fc8b7cd520316a34611535c67b3c65804221c2c5
yangkwa <yangkwa@4csoft.com>
2023-09-15 14:46:20 +0900

    첫번째 커밋

---
+++ LX2Desktop/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LX2Desktop;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ LX2Desktop/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LX2Desktop;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
